48ae6716d6b600cb754c9d9ce6d54efd5f4101e9
Stu-PID <java4first@thelewins.net>
2026-02-22 15:40:45 -0500

    Implement New Turret (not done)
    Take the new framework template and insert the Turret code from the Proto-Bot.

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -9,9 +9,19 @@
 package frc.robot.subsystems.hopper;
 
 import static frc.robot.subsystems.SubsystemConstants.hopperName;
+import static frc.robot.subsystems.hopper.HopperConstants.*;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.subsystems.ISubsystem;
 import frc.robot.subsystems.RevRoboticsSubsystem;
+import frc.robot.util.SparkUtil501;
+import org.littletonrobotics.junction.Logger;
 
 /**
  * This class contains the implementation of the <code>Hopper</code> subsystem.
@@ -24,14 +34,85 @@ import frc.robot.subsystems.RevRoboticsSubsystem;
  */
 public class Hopper extends RevRoboticsSubsystem implements ISubsystem {
 
+  /** */
+  private final SparkFlex leader;
+  /** */
+  private final SparkFlex follower;
+
+  /** */
+  private double currentSpeed;
+
   /** Constructs a new instance of the subsystem. */
   public Hopper() {
     super(hopperName);
     initConstruction();
 
+    // Create and configure leader
+    leader = new SparkFlex(leaderCanId, MotorType.kBrushless);
+    SparkFlexConfig leaderConfig = new SparkFlexConfig();
+    leaderConfig.idleMode(IdleMode.kCoast);
+    SparkUtil501.tryUntilOk(
+        leader,
+        5,
+        () ->
+            leader.configure(
+                leaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+
+    // Create and configure follower
+    follower = new SparkFlex(followerCanId, MotorType.kBrushless);
+    SparkFlexConfig followerConfig = new SparkFlexConfig();
+    followerConfig.idleMode(IdleMode.kCoast).follow(leader, followerInverted);
+    SparkUtil501.tryUntilOk(
+        follower,
+        5,
+        () ->
+            follower.configure(
+                followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+
+    // We start stopped
+    currentSpeed = 0;
+    setSpeed(currentSpeed);
+
     finishConstruction();
   }
 
+  // TODO - Document directions for +/- speed
+  /**
+   * Accepts a manual override of any defaults or PID controlled set points to allow <i>Operator</i>
+   * adjustment of the position. Positive values ???? and negative values ????.
+   *
+   * @param speed - The speed to set. Value should be between -1.0 and +1.0.
+   */
+  public void acceptTeleopInput(double speed) {
+    if (!DriverStation.isTeleopEnabled()) {
+      return;
+    }
+    currentSpeed = speed;
+  }
+
+  /**
+   * Sets the desired speed on the motor.
+   * 
+   * @param speed - The speed to set. Value should be between -1.0 and +1.0.
+   */
+  private void setSpeed(double speed) {
+    leader.set(speed);
+  }
+
   @Override
-  public void periodic() {}
+  public void disabledInit() {
+    // Ensure any motion stops when we go to <i>disabled</code>
+    setSpeed(0);
+    // TODO - Remember why we don't call motor.stop() vs. setting speed to 0 (or else fix)
+  }
+
+  // TODO - Add code to set the default speed on auto & teleop if running that way
+
+  @Override
+  public void periodic() {
+    setSpeed(currentSpeed);
+
+    Logger.recordOutput(hopperName + "/CurrentSpeed", currentSpeed);
+    Logger.recordOutput(hopperName + "/Output", leader.get());
+  }
 }
--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -17,4 +17,16 @@ package frc.robot.subsystems.hopper;
  * @author first.brian Buzzell
  * @version 2026.0.0
  */
-public final class HopperConstants {}
+public final class HopperConstants {
+
+  /** CAN ID of the leader motor. All control is done through this one. */
+  public static final int leaderCanId = 50;
+  /** CAN ID of the follower motor. */
+  public static final int followerCanId = 51;
+
+  /** Whether the <i>follower</i> needs to be reversed from the <i>leader</i>. */
+  public static final boolean followerInverted = false;
+
+  /** Default speed for the subsystem if nothing else is controlling it. */
+  public static final double defaultSpeed = 0.75;
+}
