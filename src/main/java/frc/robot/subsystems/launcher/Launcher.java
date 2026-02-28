23f7d1a0851ec5bb0a4b030fd8d846fde1bcaad3
Brian Buzzell <brianbu@powerknights.com>
2026-02-28 14:21:37 -0500

    Added Launcher Subsystem Stub

---
+++ src/main/java/frc/robot/subsystems/launcher/Launcher.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+/*------------------------------------------------------------------------*/
+/*- Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
+/*- Open Source Software - may be modified and shared by other FRC teams  */
+/*- under the terms of the Team501 license. The code must be accompanied  */
+/*- by the Team 501 - The PowerKnights license file in the root directory */
+/*- of this project.                                                      */
+/*------------------------------------------------------------------------*/
+
+
+package frc.robot.subsystems.launcher;
+
+/**
+ * This class contains the constants for the <code>Intake</code> subsystem.
+ *
+ * <p>More detail ...
+ *
+ * @since 2026.0.0
+ * @author first.brian Buzzell
+ * @version 2026.0.0
+ */
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Launcher extends SubsystemBase {
+  /** Creates a new Launcher. */
+  public Launcher() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+/*------------------------------------------------------------------------*/
+/*- Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
+/*- Open Source Software - may be modified and shared by other FRC teams  */
+/*- under the terms of the Team501 license. The code must be accompanied  */
+/*- by the Team 501 - The PowerKnights license file in the root directory */
+/*- of this project.                                                      */
+/*------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.launcher;
+
+/**
+ * This class contains the constants for the <code>Intake</code> subsystem.
+ *
+ * <p>More detail ...
+ *
+ * @since 2026.0.0
+ * @author first.brian Buzzell
+ * @version 2026.0.0
+ */
+
+ 
+/** Add your docs here. */
+public class LauncherConstants {}
