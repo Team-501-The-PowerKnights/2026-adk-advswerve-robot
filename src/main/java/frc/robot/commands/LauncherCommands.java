d253eb2ce7de4ac9e0765a497b93bcae211cb0e0
Brian Fasano <Brian@Fasano.net>
2026-03-08 15:40:08 -0400

    Standardized Debug Call and Function

--- src/main/java/frc/robot/commands/LauncherCommands.java
+++ src/main/java/frc/robot/commands/LauncherCommands.java
@@ -1,12 +1,18 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.hopper.Hopper;
 import frc.robot.subsystems.launcher.Launcher;
 import java.util.function.DoubleSupplier;
 
 public class LauncherCommands {
+
+  /** Deadband for joystick inputs */
+  private static final double DEADBAND = 0.1;
+
   public static Command joystickDrive(
       Launcher launcher, DoubleSupplier leftJoystick, Hopper hopper) {
 
@@ -40,17 +46,11 @@ public class LauncherCommands {
         launcher);
   }
 
-  public static Command debugManual(Launcher launcher, DoubleSupplier leftJoystick) {
-
-    return new RunCommand(
+  public static Command debugManual(Launcher launcher, DoubleSupplier speedSupplier) {
+    return Commands.run(
         () -> {
-          double left = leftJoystick.getAsDouble(); // -1..1
-
-          if (left > 0.05 || left < -0.05) {
-            launcher.setPercent(left);
-          } else {
-            launcher.stop();
-          }
+          double speed = MathUtil.applyDeadband(speedSupplier.getAsDouble(), DEADBAND);
+          launcher.setPercent(speed);
         },
         launcher);
   }
