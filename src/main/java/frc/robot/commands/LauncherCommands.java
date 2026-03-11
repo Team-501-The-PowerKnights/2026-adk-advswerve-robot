bc110f30ea216e45349e90c1021bb6b67f63b1b6
Stu-PID <java4first@thelewins.net>
2026-03-10 20:17:51 -0400

    Normalize Launcher
    Make it match the conventions of the subsystems.

--- src/main/java/frc/robot/commands/LauncherCommands.java
+++ src/main/java/frc/robot/commands/LauncherCommands.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.launcher.Launcher;
 import frc.robot.subsystems.launcher.LauncherConstants;
 import java.util.function.DoubleSupplier;
@@ -13,7 +12,20 @@ public class LauncherCommands {
   /** Deadband for joystick inputs */
   private static final double DEADBAND = 0.1;
 
-  public static Command joystickDrive(Launcher launcher, DoubleSupplier speedSupplier) {
+  /** Private constructor so can't be instantiated externally */
+  private LauncherCommands() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  /**
+   * @param hopper
+   * @return
+   */
+  public static Command stop(Launcher launcher) {
+    return launcher.runOnce(launcher::stop).withName("LauncherStop");
+  }
+
+  public static Command manual(Launcher launcher, DoubleSupplier speedSupplier) {
     return Commands.run(
         () -> {
           double speed = MathUtil.applyDeadband(speedSupplier.getAsDouble(), DEADBAND);
@@ -22,15 +34,11 @@ public class LauncherCommands {
         launcher);
   }
 
-  public static Command stop(Launcher launcher) {
-    return launcher.runOnce(launcher::stop).withName("LauncherStop");
-  }
-
   /**
    * @param launcher
    * @return
    */
-  public static Command LaunchIn(Launcher launcher) {
+  public static Command pullIn(Launcher launcher) {
     return launcher
         .runEnd(() -> launcher.acceptInput(-LauncherConstants.defaultSpeed), launcher::stop)
         .withName("LauncherPullIn");
@@ -40,33 +48,21 @@ public class LauncherCommands {
    * @param launcher
    * @return
    */
-  public static Command launchOut(Launcher launcher) {
+  public static Command pushOut(Launcher launcher) {
     return launcher
-        .runEnd(() -> launcher.acceptInput(+LauncherConstants.defaultSpeed), launcher::launcherIdle)
-        .withName("LauncherIdle");
+        .runEnd(() -> launcher.acceptInput(+LauncherConstants.defaultSpeed), launcher::stop)
+        .withName("LauncherPushOut");
   }
 
-  public static Command launchIdle(Launcher launcher) {
-    return launcher
-        .runEnd(() -> launcher.acceptInput(+LauncherConstants.defaultIdleSpeed), launcher::stop)
-        .withName("LauncherIdle");
-  }
-
-  public static Command joystickDrive2(Launcher launcher, DoubleSupplier leftJoystick) {
-
-    return new RunCommand(
-        () -> {
-          double left = leftJoystick.getAsDouble(); // -1..1
-
-          if (left > DEADBAND || left < -DEADBAND) {
-            launcher.acceptInput(left);
-          } else {
-            launcher.stop();
-          }
-        },
-        launcher);
-  }
-
+  /**
+   * Command to do a manual control of the subystem for debugging.
+   *
+   * <p>Every subsystem <b>has</b> to have this to allow debug.
+   *
+   * @param launcher
+   * @param speedSupplier
+   * @return
+   */
   public static Command debugManual(Launcher launcher, DoubleSupplier speedSupplier) {
     return Commands.run(
         () -> {
