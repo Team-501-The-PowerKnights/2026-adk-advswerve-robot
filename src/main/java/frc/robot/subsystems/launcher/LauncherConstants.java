b7e9882d4be0fff444fe9e021b8e6782724abd83
Brian Fasano <Brian@Fasano.net>
2026-03-14 08:16:34 -0400

    Speed Tweaks and Path add

--- src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
+++ src/main/java/frc/robot/subsystems/launcher/LauncherConstants.java
@@ -15,7 +15,7 @@ public class LauncherConstants {
 
   public static final double defaultIdleSpeed = 0.1;
 
-  public static final double kLauncherNearSpeed = 0.3;
+  public static final double kLauncherNearSpeed = 0.25;
   public static final double kLauncherMidSpeed = 0.36;
   public static final double kLauncherFarSpeed = 0.42;
   /** */
